eea2c7aa36c08496dbbf54900074b5b60a062ee9
yetimark <blaine.andrada@cuw.edu>
2018-05-24 12:11:35 -0500

    ChangeCharacter
    Change characters interactively

---
+++ Assets/Scripts/AWSDMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AWSDMove : MonoBehaviour
+{
+    private float horizontal;
+    private float vertical;
+    private float moveSpeed = 7f;
+
+	void Update ()
+    {
+        Move();
+	}
+
+    void Move()
+    {
+        this.horizontal = Input.GetAxis("Horizontal");
+        this.vertical = Input.GetAxis("Vertical");
+
+        this.transform.position += new Vector3(this.horizontal, 0, this.vertical) * Time.deltaTime * moveSpeed;
+    }
+}
