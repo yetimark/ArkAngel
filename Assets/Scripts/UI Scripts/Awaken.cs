8cb3707c9f7a6609b98a88fec7afb3847c535312
yetimark <blaine.andrada@cuw.edu>
2018-06-05 08:36:32 -0500

    Start NewCharacter
    Switch to new eden system

--- Assets/Scripts/UI Scripts/Awaken.cs
+++ Assets/Scripts/UI Scripts/Awaken.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Awaken : MonoBehaviour
+public class Awaken : MonoBehaviour  //FIX, make ghost character on start and have a ghost character allow for creation of new characters on contact
+    //new characters should be asked name, class, and customizations(should customizations happen here initially or over at the hut?)
 {
     // makes the character "wakeUp"
     public bool allowedToFill;
+    public bool allowedToClose;
     public float filled = 1f;
     public float unfilled = 0f;
     public float timeToChange = 5f; //change this val to affect speed of animation
@@ -19,6 +21,7 @@ public class Awaken : MonoBehaviour
         this.lerpedColor = this.GetComponent<Image>().color;
         //Debug.Log("a is.." + this.GetComponent<Image>().color.a);
         this.allowedToFill = false;
+        this.allowedToClose = false;
     }
 
     void Update ()
@@ -29,7 +32,7 @@ public class Awaken : MonoBehaviour
         }
 
         if (allowedToFill &&
-            GameObject.Find("UI Controller").GetComponent<UIButtonFunctions>().pauseCounter > 0)
+            GameObject.Find("UI Controller").GetComponent<UIButtonFunctions>().pauseCounter > 0) //on collision
         {
             this.allowedToFill = false;
             StartCoroutine(WakeUp(this.unfilled, this.filled));//ctrl z away
@@ -37,7 +40,7 @@ public class Awaken : MonoBehaviour
             this.currAngel.GetComponent<AWSDMove>().enabled = false;//should this be done here?
         }
 
-        if(this.lerpedColor.a >= 0.99)
+        if(this.allowedToClose)     //make happen on button click if new character // make happen after seconds for existing character
         {
             StartCoroutine(WakeUp(this.filled, this.unfilled));
             if(GameObject.Find("UI Controller").GetComponent<UIButtonFunctions>().pauseCounter > 0)
